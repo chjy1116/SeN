71f4144603f87249551d5154182a53e2e3399990
Taehoon Kim <taesic3@gamil.com>
2016-04-25 17:18:14 +0900

    sub 기능 추가
    sub 기능 추가

--- src/main/java/seNex/Calculator.java
+++ src/main/java/seNex/Calculator.java
@@ -1,13 +1,17 @@
 package seNex;
 
 public class Calculator {
-	
+
 	private int result;
-	
+
 	public void add(int x, int y) {
-		result = x+y;
+		result = x + y;
 	}
-	
+
+	public void sub(int x, int y) {
+		result = x - y;
+	}
+
 	public int getResult() {
 		return result;
 	}
